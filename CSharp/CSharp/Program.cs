adbb9384f0dcde28c5b49beca254eef3efbe44bd
immt <30967271+mt-yu@users.noreply.github.com>
2018-11-27 14:46:44 +0800

    832. 翻转图像

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -13,9 +13,26 @@ namespace CSharp
             Solution sol = new Solution();
 
 
-            /*929.独特的电子邮件*/
-            string[] emails = {"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"};
-            Console.WriteLine(sol.NumUniqueEmails(emails));
+            /*929.独特的电子邮件 测试*/
+            //string[] emails = {"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"};
+            //Console.WriteLine(sol.NumUniqueEmails(emails));
+
+            /*832. 翻转图像 测试*/
+            // 定义一个 3*3 和 4*4  的输入数组 
+            //二维数组
+            //int[,] a = new int[3, 3] { { 1, 1, 0 }, { 1, 0, 1 }, { 0, 0, 0 } };
+            //int[,] b = new int[4, 4] { { 1, 1, 0, 0 }, { 1, 0, 0, 1 }, { 0, 1, 1, 1 }, { 1, 0, 1, 0 } };
+            int i, j;
+            //交错数组
+            int[][] a = new int[][] { new int[] { 1, 1, 0 }, new int[] { 0, 0, 1 }, new int[] { 1, 0, 0 } };
+            int[][] b = new int[][] { new int[] { 1, 1, 0, 0, 1 }, new int[] { 1, 0, 0, 1 }, new int[] { 0, 1, 1, 1 }, new int[] { 1, 0, 1, 0 }, new int[] { 1, 0, 1, 0 }, new int[] { 1, 0, 1, 0 } };
+            int[][] temp = sol.FlipAndInvertImage(a);
+            for (i = 0; i <= temp.GetUpperBound(0); i++)
+            {
+                for (j = 0; j <= temp[i].GetUpperBound(0); j++)
+                    Console.Write(temp[i][j]);
+                Console.WriteLine("");
+            }
 
 
             Console.ReadKey();
---
+++ CSharp/readme.md
@@ -0,0 +1,67 @@
+
+##### 832.翻转图像      
+
+    >Array.Reverse(arr) //反转数组
+    >>arr.GetUpperBound(0) //获取二维数组的行数
+    >>>arr[0].GetUpperBound(0) //获取二维数组的列数
+
+
+
+
+
+
+******
+#### 以下为MarkDown 语法测试。。。
+---
+
+------
+
+***
+
+![blockchain](https://raw.githubusercontent.com/mt-yu/material/master/dektop1.jpg "test")
+
+
+[简书](http://jianshu.com)        
+[百度](http://baidu.com)  
+<a href="https://www.jianshu.com/u/1f5ac0cf6a8b" target="_blank">简书</a>
+
+>这是引用的内容
+>>这是引用的内容
+>>>>>>>>>>这是引用的内容
+
+- 列表内容
+    - 列表内容
+        - 列表内容
++ 列表内容
+* 列表内容
+
+1. 列表内容
+    2. 列表内容
+        3. 列表内容
+2. 列表内容  
+3. 列表内容  
+
+表头|表头|表头|表头|表头
+---|:--:|---:|---:|---:
+内容|内容|内容|内容|内容
+内容|内容|内容|内容|内容
+内容|内容|内容|内容|内容
+
+`create database hero;`
+
+```
+    function fun(){
+         echo "这是一句非常牛逼的代码";
+    }
+    fun();
+```
+
+```flow
+st=>start: 开始
+op=>operation: My Operation
+cond=>condition: Yes or No?
+e=>end
+st->op->cond
+cond(yes)->e
+cond(no)->op
+&```
