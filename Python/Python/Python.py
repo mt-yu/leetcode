aba81689574a1b1fa8da92d19ae359986dfd1309
immt <30967271+mt-yu@users.noreply.github.com>
2018-11-28 13:50:52 +0800

    461 汉明距离

--- Python/Python/Python.py
+++ Python/Python/Python.py
@@ -2,6 +2,10 @@ from Solution import Solution
 
 sol = Solution()
 
-#709.转换成小写字母
-print(sol.toLowerCase("HELLO"))
+# 709.转换成小写字母
+#print(sol.toLowerCase("HELLO"))
+
+# 461. 汉明距离
+a = sol.hammingDistance(2,5)
+print(a)
 
--- Python/Python/Solution.py
+++ Python/Python/Solution.py
@@ -3,8 +3,8 @@ class Solution(object):
     def __init__(self, *args, **kwargs):
         return super().__init__(*args, **kwargs)
 
-
-    """
+   
+    """ 709.转换成小写字母
         实现函数 ToLowerCase()，该函数接收一个字符串参数 str，
         并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
     """
@@ -14,3 +14,38 @@ class Solution(object):
         :rtype: str
         """
         return str.lower()
+
+
+    """ 461. 汉明距离
+        两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
+
+        给出两个整数 x 和 y，计算它们之间的汉明距离。
+
+        注意：
+        0 ≤ x, y < 231.
+
+        示例:
+
+        输入: x = 1, y = 4
+
+        输出: 2
+
+        解释:
+        1   (0 0 0 1)
+        4   (0 1 0 0)
+               ↑   ↑
+
+        上面的箭头指出了对应二进制位不同的位置。
+    """
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        count = 0
+        num = x^y
+        while(num != 0):
+            num = num & (num - 1)
+            count = count + 1
+        return count
